b6a501d7092c501e2992ce1c574cee358baf78ed
Andrew Johnston <apjohnsto@gmail.com>
2020-03-08 20:59:54 +0000

    clean up

--- experiments/wasm/run.js
+++
@@ -1,8 +0,0 @@
-const { evaluate, load } = just.require('wasm')
-const wasm = load('./fizzbuzz.wasm')
-const context = {
-  println: (off, len) => just.print(context.memory.buffer.readString(len, off))
-}
-const { fizzbuzz } = evaluate(wasm, context)
-fizzbuzz(16)
-just.print(just.memoryUsage().rss)
